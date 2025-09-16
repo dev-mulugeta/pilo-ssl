b32eec699e28d4c31223fd10f28b4b8355ef9c83
Mulugeta Mehari <mulugetameahri@arsiun.edu.et>
2025-09-16 22:53:13 +0300

    feat: added a new route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/light',function(){
+    return "let there be an eternal light";
+});
